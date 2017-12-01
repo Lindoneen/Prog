6494ab6e6e522a76952289c5fbcc587d7941d5df
arbuzko <firesmert@mail.ru>
2017-12-01 14:37:40 +0300

    add functios * and /

---
+++ Function3.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h" 
+#include "funcs.h" 
+#include <cmath>
+double MyMul(double a, double b) 
+{ 
+	return a * b; 
+}
---
+++ Function4.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h" 
+#include "funcs.h" 
+#include <cmath>
+double MyDiv(double a, double b) 
+{ 
+	return a / b; 
+}
