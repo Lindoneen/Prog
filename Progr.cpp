37398158d2e06fcbe46ec4ba6b42f35769a1ddd1
arbuzko <firesmert@mail.ru>
2017-12-01 13:33:25 +0300

    version 0.1

--- Progr.cpp
+++ Progr.cpp
@@ -7,7 +7,31 @@ using namespace std;
 
 int main() 
 { 
-
+switch (count) // начало оператора switch 
+	{ 
+		case 1: // если count = 1 
+			{ 
+			 // выполнить сложение 
+			break;
+			} 
+		case 2: // если count = 2 
+			{ 
+				 // выполнить вычитание 
+				break; 
+			} 
+		case 3: // если count = 3 
+			{ 
+				 // выполнить умножение 
+				break; 
+			} 
+		case 4: // если count = 4 
+			{ 
+				 // выполнить деление 
+				break; 
+			} 
+			default: // если count равно любому другому значению 
+				cout << "Nepravilni vvod" << endl; 
+	}
 
 system("pause");
 return 0; 
